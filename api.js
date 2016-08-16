946eb5a85b717f4012a36fb214045cae3aba6f0f
shinout(node-circleci-autorelease) <shinout310@gmail.com>
2016-08-16 06:41:06 +0000

    gh-pages

---
+++ api.js
@@ -0,0 +1,21 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "LoopbackClient",
+        "LoopbackPromised",
+        "LoopbackRelatedClient",
+        "LoopbackUserClient",
+        "PushManager"
+    ],
+    "modules": [
+        "loopback-promised"
+    ],
+    "allModules": [
+        {
+            "displayName": "loopback-promised",
+            "name": "loopback-promised",
+            "description": "Loopback Client to access to PersistedModel (or extenders)\n\nsee http://docs.strongloop.com/display/public/LB/PersistedModel+REST+API\nsee also http://apidocs.strongloop.com/loopback/#persistedmodel"
+        }
+    ]
+} };
+});
